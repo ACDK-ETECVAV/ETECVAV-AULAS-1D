4dc440d7a1c4666ce9f8212fc59eb6b68003bbc2
ACDK | INFONET - 1D <aleatorizando29@gmail.com>
2026-08-14 12:20:10 -0300

    Update atividade3.py

--- 1D-ACDK/Aulas/PA-Programaçao-e-Algoritmo/3BIMESTRE/LE-PYTHON/AC-PYTHON/atividade3.py
+++ 1D-ACDK/Aulas/PA-Programaçao-e-Algoritmo/3BIMESTRE/LE-PYTHON/AC-PYTHON/atividade3.py
@@ -2,7 +2,6 @@
     Autor: Calebe Barros
     Data: 14/08/2026
     Descrição: Lê um número inteiro positivo e exibe a contagem de 1 até esse número.
-    Versão adaptada a partir do exemplo em C++ preparado por Kael Elodie Whitmore.
 """
 
 numero = int(input("Digite um numero inteiro positivo: "))
