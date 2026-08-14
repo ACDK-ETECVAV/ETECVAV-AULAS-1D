91687b43cea8915bd5a50100d7d76f763439e63a
ACDK | INFONET - 1D <aleatorizando29@gmail.com>
2026-08-14 12:19:25 -0300

    Update atividade1.py

--- 1D-ACDK/Aulas/PA-Programaçao-e-Algoritmo/3BIMESTRE/LE-PYTHON/AC-PYTHON/atividade1.py
+++ 1D-ACDK/Aulas/PA-Programaçao-e-Algoritmo/3BIMESTRE/LE-PYTHON/AC-PYTHON/atividade1.py
@@ -2,7 +2,6 @@
     Autor: Calebe Barros
     Data:  14/08/2026
     Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
-    Versão adaptada a partir do exemplo em C++ preparado por Kael Elodie Whitmore.
 """
 
 numero = int(input("Digite um numero inteiro: "))
