a960703ee09bf467c933f4261852ea65d027e927
ACDK | INFONET - 1D <aleatorizando29@gmail.com>
2026-08-14 12:17:36 -0300

    Create atividade4.py

---
+++ 1D-ACDK/Aulas/PA-Programaçao-e-Algoritmo/3BIMESTRE/LE-PYTHON/AC-PYTHON/atividade4.py
@@ -0,0 +1,23 @@
+"""
+    Autor: Calebe Barros
+    Data: 14/08/2026
+    Descrição: Calcula a soma dos números pares de 1 até um número informado.
+    Versão adaptada a partir do exemplo em C++ preparado por Kael Elodie Whitmore.
+"""
+
+numero = int(input("Digite um numero inteiro positivo: "))
+contador = 1
+soma = 0
+
+if numero > 0:
+    while contador <= numero:
+
+        if contador % 2 == 0:
+            soma = soma + contador
+
+        contador += 1
+
+    print("Soma dos pares:", soma)
+
+else:
+    print("Numero invalido.")
