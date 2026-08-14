2e2294712ed9b675c7ec877114e67083679fb859
ACDK | INFONET - 1D <aleatorizando29@gmail.com>
2026-08-14 12:28:23 -0300

    Update atividade2.py

--- 1D-ACDK/Aulas/PA-Programaçao-e-Algoritmo/3BIMESTRE/LE-PYTHON/AC-PYTHON/atividade2.py
+++ 1D-ACDK/Aulas/PA-Programaçao-e-Algoritmo/3BIMESTRE/LE-PYTHON/AC-PYTHON/atividade2.py
@@ -8,7 +8,7 @@ numero1 = int(input("Digite o primeiro numero: "))
 numero2 = int(input("Digite o segundo numero: "))
 
 if numero1 > numero2:
-    print("O maior numero e:", nume/ro1)
+    print("O maior numero e:", numero1)
 elif numero2 > numero1:
     print("O maior numero e:", numero2)
 else:
