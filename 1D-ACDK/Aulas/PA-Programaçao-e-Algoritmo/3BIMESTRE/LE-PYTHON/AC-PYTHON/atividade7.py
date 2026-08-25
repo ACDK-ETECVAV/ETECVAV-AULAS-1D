6c97bb2bda4200441f9208c3985ac9f8f79fe90d
ACDK | INFONET - 1D <aleatorizando29@gmail.com>
2026-08-25 08:49:54 -0300

    Create atividade7.py

---
+++ 1D-ACDK/Aulas/PA-Programaçao-e-Algoritmo/3BIMESTRE/LE-PYTHON/AC-PYTHON/atividade7.py
@@ -0,0 +1,26 @@
+"""
+    Autor: Anthony Muraro
+    Data: 25/08/2026
+    Descrição: Calcula a média de notas válidas entre 0 e 10 usando while.
+"""
+
+contador = 1
+positivos = 0
+negativos = 0
+zeros = 0
+
+while contador <= 10:
+    numero = int(input(f"Digite o {contador}° numero: "))
+
+    if numero > 0:
+        positivos += 1
+    elif numero < 0:
+        negativos += 1
+    else:
+        zeros += 1
+
+    contador += 1
+
+print("Positivos:", positivos)
+print("Negativos:", negativos)
+print("Zeros:", zeros)
